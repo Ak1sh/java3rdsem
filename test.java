752c0cf8fdb17a153362566467776f2aa9b90b9f
Ak1sh <93367904+Ak1sh@users.noreply.github.com>
2021-12-04 10:25:15 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,45 +1,3 @@
-import java.util.*;
-public class test{
-public static void main(String args[])
-{
-int t;
-Scanner in=new Scanner(System.in);
-System.out.print("Enter the number of test cases :");
-t=in.nextInt();
-  while(t!=0)
-{
-  t--;
-  Book b1=new Book();
-  System.out.println(b1);
-}
-}     
-}
-class Book{
- String name,aurthor;
- int price,num_pages;
-
-  Book(){
-   Scanner take =new Scanner(System.in);
-   System.out.print("Enter the name of the book: ");
-   name=take.nextLine();
-   System.out.print("Enter the name of the author: ");
-   aurthor=take.nextLine();
-   System.out.print("Enter the price of the book: ");
-   price=take.nextInt();
-   System.out.print("Enter the number of pages of the book: ");
-   num_pages=take.nextInt();
-  }
-  public String toString()
-  {
-     return "name: "+name+" "+"aurthor: "+aurthor+" "+"price: "+price+" "+"num_pages: "+num_pages;
-  }
-}
-
-
-
-
-
-
 /*Create a class
 Book which contains four members: name, author, price, num_pages. Include a
 constructor to set the values for the members. Include methods to set and get
@@ -55,11 +13,11 @@ int t;
 Scanner in=new Scanner(System.in);
 System.out.print("Enter the number of books:");
 t=in.nextInt();
-  Book b[]=new int[t];
+  Book b[]=new Book[t];
   for(int i=0;i<t;i++)
   {
       b[i]=new Book();
-    b[i].get();
+      b[i].set();
   }
   for(int i=0;i<t;i++)
   {
@@ -72,11 +30,11 @@ class Book{
  int price,num_pages;
 
   Book(){
-      String name=0,aurthor=0;
+      String name=null,aurthor=null;
  int price=0,num_pages=0;
    
   }
-  void get()
+  void set()
   {
    Scanner take =new Scanner(System.in);
    System.out.print("Enter the name of the book: ");
@@ -93,8 +51,4 @@ class Book{
      return "name: "+name+" "+"aurthor: "+aurthor+" "+"price: "+price+" "+"num_pages: "+num_pages;
   }
 }
-
-
-
-
 
